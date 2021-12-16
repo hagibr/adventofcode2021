33161c01c5f9d3bbed6245d33621a03e3ff9fa3d
Alexandre Hideki Hagihara <alexandre.hagihara@gmail.com>
2021-12-16 13:45:26 -0300

    Dia 16 parte 1 OK

---
+++ 16/solve16-1.py
@@ -0,0 +1,76 @@
+data_hex = '4057231006FF2D2E1AD8025275E4EB45A9ED518E5F1AB4363C60084953FB09E008725772E8ECAC312F0C18025400D34F732333DCC8FCEDF7CFE504802B4B00426E1A129B86846441840193007E3041483E4008541F8490D4C01A89B0DE17280472FE937C8E6ECD2F0D63B0379AC72FF8CBC9CC01F4CCBE49777098D4169DE4BF2869DE6DACC015F005C401989D0423F0002111723AC289DED3E64401004B084F074BBECE829803D3A0D3AD51BD001D586B2BEAFFE0F1CC80267F005E54D254C272950F00119264DA7E9A3E9FE6BB2C564F5376A49625534C01B0004222B41D8A80008446A8990880010A83518A12B01A48C0639A0178060059801C404F990128AE007801002803AB1801A0030A280184026AA8014C01C9B005CE0011AB00304800694BE2612E00A45C97CC3C7C4020A600433253F696A7E74B54DE46F395EC5E2009C9FF91689D6F3005AC0119AF4698E4E2713B2609C7E92F57D2CB1CE0600063925CFE736DE04625CC6A2B71050055793B4679F08CA725CDCA1F4792CCB566494D8F4C69808010494499E469C289BA7B9E2720152EC0130004320FC1D8420008647E8230726FDFED6E6A401564EBA6002FD3417350D7C28400C8C8600A5003EB22413BED673AB8EC95ED0CE5D480285C00372755E11CCFB164920070B40118DB1AE5901C0199DCD8D616CFA89009BF600880021304E0EC52100623A4648AB33EB51BCC017C0040E490A490A532F86016CA064E2B4939CEABC99F9009632FDE3AE00660200D4398CD120401F8C70DE2DB004A9296C662750663EC89C1006AF34B9A00BCFDBB4BBFCB5FBFF98980273B5BD37FCC4DF00354100762EC258C6000854158750A2072001F9338AC05A1E800535230DDE318597E61567D88C013A00C2A63D5843D80A958FBBBF5F46F2947F952D7003E5E1AC4A854400404A069802B25618E008667B7BAFEF24A9DD024F72DBAAFCB312002A9336C20CE84'
+
+map_hex_bin = {
+    '0':'0000',
+    '1':'0001',
+    '2':'0010',
+    '3':'0011',
+    '4':'0100',
+    '5':'0101',
+    '6':'0110',
+    '7':'0111',
+    '8':'1000',
+    '9':'1001',
+    'A':'1010',
+    'B':'1011',
+    'C':'1100',
+    'D':'1101',
+    'E':'1110',
+    'F':'1111'
+}
+
+data_bin = ""
+for h in data_hex:
+    data_bin += map_hex_bin[h]
+
+print(data_bin)
+
+version_acc = 0
+
+def parse_packet(bdata, blen, keep_parsing):
+    global version_acc
+    pos = 0
+    while(pos < blen-6): # tem que caber pelo menos o packet version e o packet id
+        # Lendo packet Version
+        version = int( bdata[pos:pos+3], 2 )
+        print(version)
+        version_acc += version
+        pos += 3
+        # Lendo packet ID
+        id = int( bdata[pos:pos+3], 2 )
+        pos += 3
+        # Literal
+        if( id == 4 ):
+            lit_value = ''
+            while(True):
+                flag_last = int(bdata[pos:pos+1],2)
+                pos += 1
+                value = bdata[pos:pos+4]
+                pos += 4
+                lit_value += value
+                if( flag_last == 0 ):
+                    break
+        # Se não é literal, é operator
+        else: 
+            length_type = int(bdata[pos:pos+1],2)
+            pos += 1
+            if( length_type == 0): # length ocupa os próximos 15 bits
+                lensub = int(bdata[pos:pos+15],2)
+                pos += 15
+                # Agora vamos analizar o subpacket com este tamanho
+                pos += parse_packet(bdata[pos:pos+lensub],lensub,True)
+            else: # número de sub-packets ocupa os próximos 11 bits
+                numsub = int(bdata[pos:pos+11],2)
+                pos += 11
+                # Analisando pela quantidade de subpackets
+                for s in range(numsub):
+                    pos += parse_packet(bdata[pos:],blen-pos,False)
+
+        # Se for para pararmos de analizar após achar 1 pacote
+        if( not keep_parsing ):
+            break
+    return pos
+
+parse_packet(data_bin, len(data_bin), False)
+    
+print(version_acc)
